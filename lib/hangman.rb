5c27881d167d469efeb57174e6cb3ebd24ea0c89
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-27 23:02:05 +0800

    added draft game logic and draft functions

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -16,4 +16,21 @@ class Hangman
     puts "word[0] word[1] word[2] word[3] word[4] word[5] word[6] 
     word[7] word[8] word[9] word[10] word[11]"
   end
+
+  def play_game
+    loop
+
+    end
+  end
+
+  def get_player_input
+    puts 'Enter your guess'
+  end
 end
+
+# randomly selects word
+# get size of word and turn each character to element
+# create array of "_" has same number of elements as word size
+# player must input number for the position of the letter on the word
+# and the letter then give feedback if its correct
+# 
