afca4c7542cc2b5928c24039684077c514a1e968
JohnReyGimenez <gimenezjr5@gmail.com>
2025-02-28 21:47:42 +0800

    added create_array function

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,18 +2,23 @@ require 'csv'
 
 class Hangman
   def initialize
-    contents = File.read('google-10000-english-no-swears.txt ').split("\n")
-    
+    @contents = File.read('google-10000-english-no-swears.txt ').split("\n")
   end
 
   def select_word
-    valid_word = contents.select {|word| word.size >= 5 && word.size <= 12}
-    valid_word.sample
+    valid_word = @contents.select {|word| word.size >= 5 && word.size <= 12}
+    return valid_word.sample
+  end
+
+  def create_array(valid_word)
+    size = valid_word.size
+    array = Array.new(size, '_')
+    return array
   end
 
   def display_board
-    word = valid_word.chars
-    puts "word[0] word[1] word[2] word[3] word[4] word[5] word[6] 
+    create_array(select_word)
+    puts "word[] word[1] word[2] word[3] word[4] word[5] word[6] 
     word[7] word[8] word[9] word[10] word[11]"
   end
 
@@ -33,4 +38,6 @@ end
 # create array of "_" has same number of elements as word size
 # player must input number for the position of the letter on the word
 # and the letter then give feedback if its correct
-# 
+# when the player inputs the letter it gets the position(index)
+# and then uses that to turn the "_" array the element and replaces the same index 
+# basically updating the other array
