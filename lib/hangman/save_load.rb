b6a96b7bbf32573bb03436051ae32f5aac25e993
JohnReyGimenez <gimenezjr5@gmail.com>
2025-03-04 21:59:25 +0800

    added save game function

--- lib/hangman/save_load.rb
+++ lib/hangman/save_load.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 module Hangman
   class SaveLoad
+    def save_game(game_state, filename = 'hangman_save.dat')
+      file.open(filename, 'wb') do |file| # Opens file in write-binary
+        file.write(Marsha.dump(game_state)) # serializes and write game state
+      end
+      puts 'Game saved.'
+    end
   end
 end
