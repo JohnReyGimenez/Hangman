326c35206f09ca316ceeb5dc5de594006ca5f3bf
JohnReyGimenez <gimenezjr5@gmail.com>
2025-03-05 21:05:31 +0800

    fixed typos and capitalization

--- lib/hangman/save_load.rb
+++ lib/hangman/save_load.rb
@@ -3,14 +3,14 @@
 module Hangman
   class SaveLoad
     def save_game(game_state, filename = 'hangman_save.dat')
-      file.open(filename, 'wb') do |file| # Opens file in write-binary
-        file.write(Marsha.dump(game_state)) # serializes and write game state
+      File.open(filename, 'wb') do |file| # Opens file in write-binary
+        file.write(Marshal.dump(game_state)) # serializes and write game state
       end
       puts 'Game saved.'
     end
 
     def load_game(filename = 'hangman_save.dat')
-      if file.exist?(filename)
+      if File.exist?(filename)
 
         File.open(filename, 'rb') do |file| # opens file in read binary
           Marshal.load(file.read)
